b8538b87727f27e777209bd50bec9202df33ec9b
Burak Can Ates <147502052+BurakCanAtes@users.noreply.github.com>
2025-06-04 17:08:02 +0200

    [ST10-feature/categories] - create Category model

---
+++ stationery-api/models/Category.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      unique: true,
+      lowercase: true,
+      minlength: [2, "Name must be at least 2 characters"],
+      maxlength: [50, "Name must be at most 50 characters"],
+    },
+  },
+  { timestamps: true }
+);
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
