412b867c6a2a549874e630ec54917751e84c94c0
Ahmet Yazgan <ahmeteminyazgan5858@gmail.com>
2025-06-09 00:25:20 +0200

    [ST9-feature/user-addresses] - create delete address function

--- stationery-api/controllers/user-address-controller.js
+++ stationery-api/controllers/user-address-controller.js
@@ -1,6 +1,7 @@
 const { createValidationError } = require("../utils/errors");
 const { createNewAddress, updateExistingAddress } = require("../services/user-address-service");
 const { findUserById } = require("../utils/shared-utils");
+const { findAddressById, sortByDefault } = require("../utils/address-utils");
 
 const getAllAddresses = async (req, res, next) => {
   try {
@@ -49,4 +50,33 @@ const updateAddress = async (req, res, next) => {
   }
 }
 
-module.exports = { getAllAddresses, addNewAddress, updateAddress };
+const deleteAddress = async (req, res, next) => {
+  try {
+    const { userId } = req;
+    const { addressId } = req.params;
+
+    const user = await findUserById(userId);
+    const address = findAddressById(user.addresses, addressId);
+
+    const updatedAddresses = user.addresses.filter(ad => !ad._id.equals(addressId));
+    if(address.isDefault && updatedAddresses.length) {
+      updatedAddresses[0].isDefault = true;
+    }
+
+    user.addresses = sortByDefault(updatedAddresses);
+    await user.save();
+
+    res.status(200).json({
+      addresses: user.addresses
+    });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+module.exports = {
+  getAllAddresses,
+  addNewAddress,
+  updateAddress,
+  deleteAddress,
+};
--- stationery-api/routes/user-address-routes.js
+++ stationery-api/routes/user-address-routes.js
@@ -7,6 +7,6 @@ const router = Router();
 router.get("", addressesController.getAllAddresses);
 router.post("", addressesController.addNewAddress);
 router.patch("/:addressId", addressesController.updateAddress);
-router.delete("/:addressId", (req, res) => res.send("Delete one address")); // TODO: delete an address
+router.delete("/:addressId", addressesController.deleteAddress);
 
 module.exports = router;
