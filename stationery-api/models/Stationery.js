71483e0acb7610f9c686511ff80c2d76869b7386
Ahmet Emin Yazgan <130467590+AhmetYazgan@users.noreply.github.com>
2025-06-15 21:15:46 +0200

    ST12/Feature - Products (#17)
    * [ST12-feature/products] - create products route
    
    * [ST12-feature/products] - create add product and get all products methods, implement filter and sort query, fix category service naming
    
    * [ST12-feature/products] - create get single product method, fix helper function import
    
    * [ST12-feature/products] - create update product method, handle category change cases too, trim string fields of models
    
    * [ST12-feature/products] - create delete product method
    
    * [ST12-feature/products] - product-controller name fixed

--- stationery-api/models/Stationery.js
+++ stationery-api/models/Stationery.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
-const Product = require("./product");
+const Product = require("./Product");
 
 const stationerySchema = new mongoose.Schema({
   color: {
     type: String,
     maxlength: [20, "Color name must be at most 20 characters"],
+    trim: true,
   },
   brand: {
     type: String,
     maxlength: [30, "Brand name must be at most 30 characters"],
     required: [true, "Brand is required"],
+    trim: true,
   },
 });
 
