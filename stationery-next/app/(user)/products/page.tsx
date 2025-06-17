f75c9d903829770033fb13ea5cd4c2a3daaa0499
Burak Can Ates <147502052+BurakCanAtes@users.noreply.github.com>
2025-06-17 21:15:51 +0200

    ST24/Feature - Pagination (#25)
    * [ST24-feature/pagination] - create pagination for products page
    
    * [ST24-feature/pagination] - remove leftover

--- stationery-next/app/(user)/products/page.tsx
+++ stationery-next/app/(user)/products/page.tsx
@@ -1,3 +1,4 @@
+import Paging from "@/components/common/Paging";
 import SortDropdown from "@/components/common/SortDropdown";
 import ProductFilterForm from "@/components/forms/ProductFilterForm";
 import ProductCard from "@/components/products/ProductCard";
@@ -14,6 +15,8 @@ export default async function Products(props: { searchParams: SearchParams }) {
   );
   const products = await getProducts(searchParams);
 
+  const { page, totalPages } = products;
+
   return (
     <div className="mx-auto max-w-7xl p-4 sm:p-6 md:py-8">
       <header className="flex items-center justify-between">
@@ -21,13 +24,16 @@ export default async function Products(props: { searchParams: SearchParams }) {
           {category.name ? capitalizeFirstLetter(category.name) : "Products"}
         </h1>
         <SortDropdown />
-      </header>
+      </header>
       <ProductFilterForm categories={categories.data} />
       <main className="mt-4 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2 sm:gap-4">
         {products.data.map((product) => (
           <ProductCard key={product._id} product={product} />
         ))}
       </main>
+      <div className="mt-6 mb-2">
+        <Paging page={page} totalPages={totalPages} searchParams={searchParams} />
+      </div>
     </div>
   );
 }
