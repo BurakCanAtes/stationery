b292024c82c6bed576c2afe2d65a4c49eb62723e
talha <atercan0658@gmail.com>
2025-06-18 11:32:02 +0200

    [ST29-feature/user-cart] - display the sub info of the product in its place

--- stationery-next/components/cart/CartItem.tsx
+++ stationery-next/components/cart/CartItem.tsx
@@ -1,20 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import { ProductInCart } from "@/lib/types/cart.type";
 import { formatAmount } from "@/lib/utils/helperFunctions";
 import QuantityButtons from "../common/buttons/QuantityButtons";
+import { ProductTypes } from "@/lib/types/product.type";
 
 const CartItem = ({ item }: { item: ProductInCart }) => {
-  const {
-    quantity,
-    product: { _id, images, name, productType, price, stock },
-  } = item;
+  const { quantity, product } = item;
+
+  const { _id, images, name, productType, price, stock } = product;
+
   const thumbnail = images[0] || "/images/galleryIcon.svg";
+
+  const productInfo =
+    productType === ProductTypes.STATIONERY
+      ? { info1: product.color, info2: product.brand }
+      : productType === ProductTypes.BOOK
+      ? { info1: product.author, info2: product.publisher }
+      : productType === ProductTypes.TOY
+      ? { info1: product.ageRange, info2: product.brand }
+      : undefined;
 
   return (
     <div className="grid grid-cols-3 lg:grid-cols-6">
       <div className="flex gap-2 col-span-2 md:gap-3 lg:col-span-3">
-        <div className="relative w-1/3 aspect-[3/4] lg:w-1/4">
+        <Link
+          href={`/products/${_id}`}
+          className="relative w-1/3 aspect-[3/4] lg:w-1/4"
+        >
           <Image
             src={thumbnail}
             alt={name}
@@ -22,7 +36,7 @@ const CartItem = ({ item }: { item: ProductInCart }) => {
             priority
             className="object-contain"
           />
-        </div>
+        </Link>
         <div className="h-full w-full flex flex-1 shrink-0 flex-col justify-evenly gap-2 lg:gap-0">
           <div className="flex flex-col">
             <p className="text-xs text-neutral-500 font-semibold">
@@ -34,10 +48,10 @@ const CartItem = ({ item }: { item: ProductInCart }) => {
           </div>
           <div className="flex flex-col gap-1">
             <p className="text-xs md:text-sm text-neutral-700 font-semibold">
-              Author or Color or Age
+              {productInfo?.info1 || ""}
             </p>
             <p className="text-xs md:text-sm text-neutral-700 font-semibold">
-              Publisher or Brand
+              {productInfo?.info2 || ""}
             </p>
           </div>
         </div>
